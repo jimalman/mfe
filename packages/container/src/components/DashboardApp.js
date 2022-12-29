9805c64117c1fbbfb1c88202229cc8762569cfd5
Jim Alman <jimalman@Jims-MacBook-Pro.local>
2022-12-29 15:02:51 -0500

    fixed dashboard config

--- packages/container/src/components/DashboardApp.js
+++ packages/container/src/components/DashboardApp.js
@@ -1,6 +1,7 @@
 import { mount } from "dashboard/DashboardApp";
 import React, { useRef, useEffect } from "react";
 
+// the dashboard app component is created in Vue
 export default () => {
   const ref = useRef(null);
 
